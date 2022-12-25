464090156e5ee37fbc24237c9450d4b2e81bb9e6
David Long <braveesnow@outlook.com>
2022-12-25 16:00:43 -0500

    Allow HTTP(S) only

--- src/commands/utilities/media.ts
+++ src/commands/utilities/media.ts
@@ -6,15 +6,18 @@ const urlOption: string = "url";
 export default {
     data: new SlashCommandBuilder()
         .setName("media")
-        .setDescription("Upload a file from a link.")
+        .setDescription("Upload a file from an HTTP(S) link.")
         .addStringOption((option) => option.setName("url").setDescription("The URL to the file.").setRequired(true)),
     execute: async (int: ChatInputCommandInteraction<CacheType>) => {
         const url: string = int.options.getString(urlOption, true);
-        const f = new AttachmentBuilder(url);
+        if (!url.startsWith("http")) {
+            await int.reply("I can only accept an HTTP or HTTPS link, guardian.").catch(logError);
+            return;
+        }
 
         await int
             .reply({
-                files: [f],
+                files: [new AttachmentBuilder(url)],
             })
             .catch(logError);
     },
